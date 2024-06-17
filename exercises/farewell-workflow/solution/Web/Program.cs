2b765b4dabb27996ced2dd5d9189fb9584a8617b
Jwahir Sundai <jwahir.sundai@temporal.io>
2024-06-17 10:14:26 -0500

    revert

--- exercises/farewell-workflow/solution/Web/Program.cs
+++ exercises/farewell-workflow/solution/Web/Program.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.AddSimpleConsole().SetMinimumLevel(LogLevel.Information);
-var logger = builder.Logging.CreateLogger("SpanishGreetingApp");
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSimpleConsole(options =>
+{
+    options.IncludeScopes = true;
+    options.SingleLine = true;
+    options.TimestampFormat = "hh:mm:ss ";
+}).SetMinimumLevel(LogLevel.Information);
 
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
 app.MapGet("/get-spanish-greeting", (HttpContext context) =>
 {
@@ -16,7 +25,7 @@ app.MapGet("/get-spanish-greeting", (HttpContext context) =>
         var name = query[0];
         var greeting = $"¡Hola, {name}!";
         logger.LogInformation($"Returning Spanish greeting: {greeting}");
-        return Results.Ok(greeting);  // Return HTTP response with greeting
+        return Results.Ok(greeting);
     }
     else
     {
@@ -32,7 +41,7 @@ app.MapGet("/get-spanish-farewell", (HttpContext context) =>
         var name = query[0];
         var farewell = $"¡Adiós, {name}!";
         logger.LogInformation($"Returning Spanish farewell: {farewell}");
-        return Results.Ok(farewell);  // Return HTTP response with farewell
+        return Results.Ok(farewell);
     }
     else
     {
