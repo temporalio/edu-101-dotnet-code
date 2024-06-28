fc4de19a0187fc6f45addd90a419fee7a925c025
Jwahir Sundai <jwahir.sundai@temporal.io>
2024-06-28 11:08:56 -0500

    changes given directory.build.props

--- exercises/farewell-workflow/solution/Client/Program.cs
+++ exercises/farewell-workflow/solution/Client/Program.cs
@@ -8,20 +8,15 @@ var client = await TemporalClient.ConnectAsync(new("localhost:7233"));
 // Generate a unique Workflow ID
 var workflowId = $"greeting-{Guid.NewGuid()}";
 
-// Prompt the user to enter their name and default to Temporal
-// Check if the name is provided as a command-line argument
 var name = args.FirstOrDefault();
 
 if (string.IsNullOrEmpty(name))
 {
-    // Prompt the user to enter their name if not provided in args
     Console.WriteLine("Please enter your name:");
     name = Console.ReadLine();
-    if (string.IsNullOrEmpty(name))
-    {
-        name = "Temporal"; // Default to "Temporal" if the input is still empty
-    }
 }
+
+name = string.IsNullOrEmpty(name) ? "Temporal" : name;
 
 // Start the Workflow
 var result = await client.ExecuteWorkflowAsync(
